348a3c22a0e61805764daaa3daf11728ad895b77
Abejide Ayodele <abejideayodele@gmail.com>
2014-01-03 10:16:39 -0600

    passed all tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "UserPages" do
   subject { page } 
 
-  describe  "signup page" do 
+  context  "signup page" do 
       before {visit signup_path}
       it {should have_content('Sign up')}
       it {should have_title(full_title('Sign up'))}
